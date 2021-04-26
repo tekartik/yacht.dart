9d806da2212e365d1b483daa9705e2598a52021e
alex <alex@tekartik.com>
2021-04-26 07:55:44 +0200

    fix lints

--- test/test_common.dart
+++ test/test_common.dart
@@ -1,4 +1,4 @@
 //import 'package:yacht/src/common_import.dart';
-export 'package:yacht/src/common_import.dart';
 //import 'package:dev_test/test.dart';
 export 'package:dev_test/test.dart';
+export 'package:yacht/src/common_import.dart';
